1ac52d6e2a04293e056a45c6c657c4c76c0db4d7
martinezdavid <1817254192go@gmail.com>
2024-11-20 10:42:27 +0800

    增加延迟

--- src/auto_download/auto_download.py
+++ src/auto_download/auto_download.py
@@ -319,7 +319,7 @@ class XKW:
         try:
             # 尝试找到页面上的“登录”按钮或链接
             tab.get('https://www.zxxk.com')
-            time.sleep(5)
+            time.sleep(20)
             login_element = tab.ele('text:登录', timeout=5)
             if login_element:
                 return False
